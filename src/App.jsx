f3d56054b01c7c9bf47144d24bbb9d875c9d0775
joeabrahamk <joeabrahamk2004@gmail.com>
2024-09-19 22:13:52 +0530

    first iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Hero from './Components/Hero.jsx';
+import About from './Components/About.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <Hero />
+    <About />
     </>
   )
 }
---
+++ src/Components/About.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+function About() {
+return (
+    <>
+
+        <div className="About-container">
+            <h1>About</h1>
+
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, et,
+          distinctio eum impedit nihil ipsum modi. kytdyzh yfiflci utddk ktuex cg tdekeyeh the afeul, akfuvf hugh buoo. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, et,
+          distinctio eum impedit nihil ipsum modi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, etdistinctio eum impedit nihil ipsum modi. ipsum, dolor sit amet consectetur adipisicing elit. Quae, et,
+          distinctio eum impedit nihil ipsum modi. kytdyzh yfiflci utddk ktuex cg tdekeyeh the afeul, akfuvf hugh buoo. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, et,
+          distinctio eum impedit nihil ipsum modi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, etdistinctio eum impedit nihil ipsum modi.</p>
+
+          
+          
+          <hr className='abthr'></hr>
+          <hr></hr>
+          <hr></hr>
+
+        </div>
+    
+    </>
+
+
+);
+}
+
+export default About;
